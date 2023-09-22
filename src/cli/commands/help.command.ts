9eb1d8d83e02cbf28d8d81eaa4b20230623b8e4b
vindimm <vindimm@bk.ru>
2023-09-22 12:10:04 +0500

    Добавил цветной вывод в консоль для команды --help

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { Command } from './command.interface.js';
 
 export class HelpCommand implements Command {
@@ -7,14 +8,14 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-      Программа для подготовки данных для REST API сервера.
-      Пример:
-        cli.js --<command> [--arguments]
-      Команды:
-        --version:                  # выводит номер версии
-        --help:                     # печатает этот текст
-        --import <path>:            # импортирует данные из TSV
-        --generate <n> <path> <url> # генерирует произвольное количество текстовых данных
+        Программа для подготовки данных для REST API сервера.
+        Пример:
+            ${chalk.green('cli.js --<command> [--arguments]')}
+        Команды:
+            ${chalk.green('--version:')}                   ${chalk.gray('# выводит номер версии')}
+            ${chalk.green('--help:')}                      ${chalk.gray('# печатает этот текст')}
+            ${chalk.green('--import <path>:')}             ${chalk.gray('# импортирует данные из TSV')}
+            ${chalk.green('--generate <n> <path> <url>')}  ${chalk.gray('# генерирует произвольное количество тестовых данных')}
     `);
   }
 }
