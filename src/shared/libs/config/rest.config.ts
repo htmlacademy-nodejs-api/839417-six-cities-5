9922a5dbe54bab13644fbd67f1ae154c8d8a6852
vindimm <vindimm@bk.ru>
2023-10-01 15:58:58 +0500

    Улучшил работу приложения с переменными окружения.
    Добавил схему конфигурации, в которой определены переменные, необходимые для работы приложения.
    Пакет `.env` помогает считывать данные из env-файла.
    Пакет `convict` помогает работать со схемой конфигурации.

--- src/shared/libs/config/rest.config.ts
+++ src/shared/libs/config/rest.config.ts
@@ -1,11 +1,28 @@
+import { config } from 'dotenv';
 import { Config } from './index.js';
+import { Logger } from '../logger/index.js';
+import { configRestSchema, RestSchema } from './rest.schema.js';
 
-export class RestConfig implements Config {
+export class RestConfig implements Config<RestSchema> {
+  private readonly config: RestSchema;
+
   constructor (
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: Logger
+  ) {
+    const parsedOutput = config();
 
-  public get(key: string): string | undefined {
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
