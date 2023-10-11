7fa1f1275b2ab0cb165323f9a3c41e4f8762579d
vindimm <vindimm@bk.ru>
2023-10-11 15:44:38 +0500

    обновил команду --import

--- src/shared/modules/user/dto/create-user.dto.ts
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -3,7 +3,7 @@ import { UserRank } from '../../../types/index.js';
 export class CreateUserDto {
   public name: string;
   public email: string;
-  public avatarPath: string;
+  public avatarPath?: string;
   public password: string;
   public rank: UserRank;
 }
